ebdfa6d9e0a09605011d352037b19a0bfa82173e
Nikhil Gola <nikkstheroc95@gmail.com>
2016-12-22 19:36:23 +0530

    First commit

--- app/src/main/java/com/prognoobie/nikhil/programc/CodingActivity.java
+++ app/src/main/java/com/prognoobie/nikhil/programc/CodingActivity.java
@@ -9,6 +9,9 @@ import android.widget.Button;
 import android.widget.EditText;
 import android.widget.LinearLayout;
 import android.widget.ListView;
+import android.widget.TextView;
+
+import java.io.IOException;
 import java.util.ArrayList;
 import java.util.List;
 import java.util.StringTokenizer;
@@ -16,109 +19,40 @@ import java.util.StringTokenizer;
 public class CodingActivity extends AppCompatActivity {
 
     static ArrayList<String> al;
-    ArrayList listArray = null;
+    ArrayList<String> listArray = null;
     String searchString;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_coding);
-        final EditText searchBar = (EditText) findViewById(R.id.coding_searchBar);
-        final Button searchView = (Button) findViewById(R.id.coding_search);
+        //ListView
+        ListView listView = (ListView) findViewById(R.id.programList);
+        listArray = this.getListPrograms();
         LinearLayout codingLayout = (LinearLayout) findViewById(R.id.coding_program);
-        //initials();
-        al = new ArrayList<>();
-        searchBar.setOnClickListener(new View.OnClickListener() {
-            @Override
-            public void onClick(View view) {
-                searchBar.setText("");
-            }
-        });
-        searchView.setOnClickListener(new View.OnClickListener() {
-            @Override
-            public void onClick(View view) {
+        ArrayAdapter<String> adapter = new ArrayAdapter<String>(this,R.layout.list_view_type,listArray);
+        listView.setAdapter(adapter);
 
-                searchString = searchBar.getText().toString();
-                ArrayAdapter adapter=null;
-                ListView listView =null;
-                        listView=(ListView) findViewById(R.id.programList);
-               if(listArray!=null)
-                    listArray.clear();
-                listArray=getListPrograms(searchString);
-
-                adapter = new ArrayAdapter<String>(getBaseContext(), R.layout.list_view_type, listArray);
-                listView.setAdapter(adapter);
-                adapter.notifyDataSetChanged();
-            }
-        });
 
     }
 
 
 
     //Array for ListView
-    ArrayList  getListPrograms(String searchString)
-    {
-        ArrayList<String> al1 = new ArrayList<>();
-        String[] programsInAssests;
-        ArrayList<String> nothing=new ArrayList<>();
-        nothing.add("NO MATCH FOUND");
+    ArrayList<String>  getListPrograms()
+    {   al = new ArrayList<>();
+        ArrayList<String> arrayList = new ArrayList<>();
         try {
-            programsInAssests = getAssets().list("Programs");
-            for(int i=0;i<programsInAssests.length;i++)
+            String[] listOfPrograms = this.getAssets().list("Programs");
+            for(int i=0;i<listOfPrograms.length;i++)
             {
-                al.add(programsInAssests[i]);
-                al1.add(programsInAssests[i]);
+                al.add(listOfPrograms[i]);
+                arrayList.add(listOfPrograms[i]);
             }
-
-        if(searchString.equals(""))
-        {
-
-            return al1;
-        }
-        else {
-            ArrayList<String> arr = new ArrayList<String>();
-            String[] ar;
-            String temp;
-            StringTokenizer st ;
-            boolean flag;
-            boolean searchFlag = false;
-            for(int i=0,j=0;i<al.size();i++)
-            {   flag=false;
-                temp =al.get(i);
-                st=new StringTokenizer(temp," ");
-                while(st.hasMoreTokens())
-                {
-                    if(temp.contains(st.nextToken()))
-                    {
-                        flag=true;
-                        searchFlag=true;
-                    }
-                }
-
-                if(flag)
-                {
-                    arr.add( al.get(i));
-                }
-
-
-
-            }
-            if(!searchFlag) {
-               arr = new ArrayList<String>();
-                arr.add("NO MATCH FOUND");
-            }
-
-            return arr;
-
-        }
-
-               }
-        catch(Exception e)
-        {
+        } catch (IOException e) {
             e.printStackTrace();
         }
-        return nothing;
+        return arrayList;
     }
 
 
