cfac32d7bb1f1f4a07cf2211a3aa09b805701c51
Nikhil Gola <nikkstheroc95@gmail.com>
2016-12-23 00:34:01 +0530

    Save Program option and potrait

--- app/src/main/java/com/prognoobie/nikhil/programc/CodingActivity.java
+++ app/src/main/java/com/prognoobie/nikhil/programc/CodingActivity.java
@@ -2,16 +2,20 @@ package com.prognoobie.nikhil.programc;
 
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.view.MotionEvent;
 import android.view.View;
 import android.widget.AdapterView;
 import android.widget.ArrayAdapter;
 import android.widget.Button;
 import android.widget.EditText;
+import android.widget.ImageView;
 import android.widget.LinearLayout;
 import android.widget.ListView;
 import android.widget.TextView;
 import android.widget.Toast;
 
+import java.io.File;
+import java.io.FileWriter;
 import java.io.IOException;
 import java.util.ArrayList;
 import java.util.List;
@@ -20,9 +24,14 @@ import java.util.StringTokenizer;
 public class CodingActivity extends AppCompatActivity {
 
     static ArrayList<String> al;
+    static String filename="";
+    static String str = "";
     ArrayList<String> listArray = null;
     ProgramWriter writer;
+
     String[] program = new String[3];
+
+    boolean downloadFlag= false;
 
 
     @Override
@@ -40,9 +49,14 @@ public class CodingActivity extends AppCompatActivity {
         }
         final ListView listView = (ListView) findViewById(R.id.programList);
         listArray = this.getListPrograms();
+
         final LinearLayout codingLayout = (LinearLayout) findViewById(R.id.coding_program);
+        final ImageView downloadImage = (ImageView) findViewById(R.id.download_code);
+
         ArrayAdapter<String> adapter = new ArrayAdapter<>(this,R.layout.list_view_type,listArray);
+
         listView.setAdapter(adapter);
+
         adapter.notifyDataSetChanged();
 
         final TextView header= (TextView) findViewById(R.id.program_header);
@@ -63,6 +77,7 @@ public class CodingActivity extends AppCompatActivity {
 
                 writer=new ProgramWriter(CodingActivity.this);
 
+                downloadImage.setVisibility(View.VISIBLE);
                 listView.setVisibility(View.GONE);
                 codingLayout.setVisibility(View.VISIBLE);
 
@@ -76,12 +91,50 @@ public class CodingActivity extends AppCompatActivity {
                 header.setText(program[0]);
                 mainProgram.setText(program[1]);
                 outPut.setText(program[2]);
-
+                filename=program[0]+".c";
+                str=program[1];
 
             }
         });
+       downloadImage.setOnClickListener(new View.OnClickListener() {
+           @Override
+           public void onClick(View view) {
+               if(!downloadFlag) {
+                   Toast.makeText(CodingActivity.this, "Press once more to download C file", Toast.LENGTH_LONG).show();
+                    downloadFlag = true;
+               }
+                   else
+               {
+                   downloadFlag=false;
 
 
+                   File mydir = new File("/sdcard/CPrograms/"); //Creating an internal dir;
+                   if (!mydir.exists())
+                   {
+                       mydir.mkdirs();
+                       Toast.makeText(CodingActivity.this, "Directory created",
+                               Toast.LENGTH_SHORT).show();
+                   }
+
+                   try{
+                       File cFile = new File(mydir, filename);
+                       FileWriter writer = new FileWriter(cFile);
+                       writer.append(str);
+                       writer.flush();
+                       writer.close();
+                       Toast.makeText(CodingActivity.this, "File saved successfully!",
+                               Toast.LENGTH_LONG).show();
+
+                   }catch (Exception e){
+
+                   }
+
+
+
+
+               }
+           }
+       });
     }
 
 
