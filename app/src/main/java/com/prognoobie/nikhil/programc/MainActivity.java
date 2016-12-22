d6305ccf1a96cd4b4067e7454bdadda6d4b8aea2
Nikhil Gola <nikkstheroc95@gmail.com>
2016-12-23 00:52:33 +0530

    Save Program option and potrait

--- app/src/main/java/com/prognoobie/nikhil/programc/MainActivity.java
+++ app/src/main/java/com/prognoobie/nikhil/programc/MainActivity.java
@@ -65,7 +65,7 @@ public class MainActivity extends AppCompatActivity {
         fab.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View view) {
-                Snackbar.make(view, "Replace with your own action", Snackbar.LENGTH_LONG)
+                Snackbar.make(view, "https://github.com/nikks95/CPrograms-v2 ,for Code of this app", Snackbar.LENGTH_LONG)
                         .setAction("Action", null).show();
             }
         });
